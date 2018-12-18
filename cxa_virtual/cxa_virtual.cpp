264a1ca2ffb1d776f9be15b957a3a025cf3e6257
Hyeonki Hong <hhk7734@gmail.com>
2018-12-19 05:29:41 +0900

    주석

--- cxa_virtual/cxa_virtual.cpp
+++ cxa_virtual/cxa_virtual.cpp
@@ -1,11 +1,14 @@
+/**
+ * @file cxa_virtual.cpp
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief 가상 함수관련 문제 해결을 위한 소스
+ */
+
 #include <stdlib.h>
 
 extern "C" void __cxa_pure_virtual( void ) __ATTR_NORETURN__;
 extern "C" void __cxa_deleted_virtual( void ) __ATTR_NORETURN__;
 
-/**
- * @brief 순수 가상 함수를 생성했을 때, 파생 클래스가 함수를 정의하지 않은 경우 문제 처리(?)
- */
 void __cxa_pure_virtual( void ) { abort(); }
 
 void __cxa_deleted_virtual( void ) { abort(); }
