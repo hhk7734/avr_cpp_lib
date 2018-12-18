0e2995f5e3850b56b9282e72fb469b6a7fc8a778
Hyeonki Hong <hhk7734@gmail.com>
2018-12-19 04:52:50 +0900

    주석

--- cxa_virtual/cxa_virtual.cpp
+++ cxa_virtual/cxa_virtual.cpp
@@ -1,12 +1,11 @@
 #include <stdlib.h>
 
-extern "C" void __cxa_pure_virtual(void) __ATTR_NORETURN__;
-extern "C" void __cxa_deleted_virtual(void) __ATTR_NORETURN__;
+extern "C" void __cxa_pure_virtual( void ) __ATTR_NORETURN__;
+extern "C" void __cxa_deleted_virtual( void ) __ATTR_NORETURN__;
 
-void __cxa_pure_virtual(void) {
-    abort();
-}
+/**
+ * @brief 순수 가상 함수를 생성했을 때, 파생 클래스가 함수를 정의하지 않은 경우 문제 처리(?)
+ */
+void __cxa_pure_virtual( void ) { abort(); }
 
-void __cxa_deleted_virtual(void) {
-    abort();
-}
+void __cxa_deleted_virtual( void ) { abort(); }
