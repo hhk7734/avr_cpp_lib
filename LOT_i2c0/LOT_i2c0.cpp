37dcac99caa1277041274b34c51dd08aa1c81a91
Hyeonki Hong <hhk7734@gmail.com>
2018-12-23 03:19:29 +0900

    변수 상수 함수 선언

--- LOT_i2c0/LOT_i2c0.cpp
+++ LOT_i2c0/LOT_i2c0.cpp
@@ -1,12 +1,68 @@
 /**
  * @file LOT_i2c0.cpp
  * @author Hyeon-ki, Hong (hhk7734@gmail.com)
- * @brief
+ * @brief i2c 마스터 통신
  */
 
 #include "LOT_i2c0.h"
 
-LOT_i2c0::LOT_i2c0()
-{
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
 
+volatile uint8_t &twbr { TWBR };
+volatile uint8_t &twsr { TWSR };
+volatile uint8_t &twdr { TWDR };
+volatile uint8_t &twcr { TWCR };
+
+/// TWSR
+const uint8_t tws_mask { 0xF8 };
+const uint8_t twps1 { _BV( TWPS1 ) };
+const uint8_t twps0 { _BV( TWPS0 ) };
+
+/// TWCR
+const uint8_t twint { _BV( TWINT ) };
+const uint8_t twea { _BV( TWEA ) };
+const uint8_t twsta { _BV( TWSTA ) };
+const uint8_t twsto { _BV( TWSTO ) };
+const uint8_t twwc { _BV( TWWC ) };
+const uint8_t twen { _BV( TWEN ) };
+const uint8_t twie { _BV( TWIE ) };
+
+void LOT_i2c0::twi_isr( void ) {}
+
+void LOT_i2c0::setup( const uint32_t scl_clock ) {}
+
+void LOT_i2c0::transmit( const uint8_t device_address, const uint8_t *data, uint8_t size ) {}
+void LOT_i2c0::transmit( const uint8_t device_address,
+                         const uint8_t register_address,
+                         uint8_t       data )
+{
 }
+uint8_t LOT_i2c0::receive( const uint8_t device_address, uint8_t *data, uint8_t max_size ) {}
+uint8_t LOT_i2c0::receive( const uint8_t device_address,
+                           const uint8_t register_address,
+                           uint8_t *     data,
+                           uint8_t       max_size )
+{
+}
+uint8_t LOT_i2c0::receive( const uint8_t device_address, const uint8_t register_address ) {}
+
+void LOT_i2c0::transmit_slow( const uint8_t device_address, const uint8_t *data, uint8_t size ) {}
+void LOT_i2c0::transmit_slow( const uint8_t device_address,
+                              const uint8_t register_address,
+                              uint8_t       data )
+{
+}
+uint8_t LOT_i2c0::receive_slow( const uint8_t device_address, uint8_t *data, uint8_t max_size ) {}
+uint8_t LOT_i2c0::receive_slow( const uint8_t device_address,
+                                const uint8_t register_address,
+                                uint8_t *     data,
+                                uint8_t       max_size )
+{
+}
+uint8_t LOT_i2c0::receive_slow( const uint8_t device_address, const uint8_t register_address ) {}
+
+LOT_i2c0 i2c0;
+
+ISR( TWI_vect ) { i2c0.twi_isr(); }
--- LOT_i2c0/LOT_i2c0.h
+++ LOT_i2c0/LOT_i2c0.h
@@ -1,7 +1,7 @@
 /**
  * @file LOT_i2c0.h
  * @author Hyeon-ki, Hong (hhk7734@gmail.com)
- * @brief
+ * @brief i2c 마스터 통신
  */
 
 #ifndef _LOT_I2C0_H_
@@ -9,14 +9,65 @@
 
 #include <inttypes.h>
 
+#if !defined( __AVR_ATmega328P__ )
+#    warning "Untested device"
+#endif
+
+#define LOT_I2C0_DEFAULT_CLOCK 400000UL
+
+/// 버퍼 크기는 32, 64, 128 ... 2의 지수로 설정
+#define LOT_I2C0_BUF_SIZE 32
+
+#define LOT_I2C0_TRANSFER_INTERVAL_US 1000
+
+#ifndef _LOT_STATUS_TYPEDEF_
+#    define _LOT_STATUS_TYPEDEF_
+typedef enum
+{
+    LOT_OK = 0x00,
+    LOT_ERROR,
+    LOT_BUSY,
+    LOT_TIMEOUT
+} LOT_status_typedef;
+#endif
+
 class LOT_i2c0 {
 public:
     LOT_i2c0();
 
+    void twi_isr( void );
+
+    void        setup( const uint32_t scl_clock );
+    inline void setup( void ) { setup( LOT_I2C0_DEFAULT_CLOCK ); }
+
+    void    transmit( const uint8_t device_address, const uint8_t *data, uint8_t size );
+    void    transmit( const uint8_t device_address, const uint8_t register_address, uint8_t data );
+    uint8_t receive( const uint8_t device_address, uint8_t *data, uint8_t max_size );
+    uint8_t receive( const uint8_t device_address,
+                     const uint8_t register_address,
+                     uint8_t *     data,
+                     uint8_t       max_size );
+    uint8_t receive( const uint8_t device_address, const uint8_t register_address );
+
+    void transmit_slow( const uint8_t device_address, const uint8_t *data, uint8_t size );
+    void
+            transmit_slow( const uint8_t device_address, const uint8_t register_address, uint8_t data );
+    uint8_t receive_slow( const uint8_t device_address, uint8_t *data, uint8_t max_size );
+    uint8_t receive_slow( const uint8_t device_address,
+                          const uint8_t register_address,
+                          uint8_t *     data,
+                          uint8_t       max_size );
+    uint8_t receive_slow( const uint8_t device_address, const uint8_t register_address );
+
 protected:
+    volatile uint8_t buf_index;
+    volatile uint8_t buf_length;
+
+    volatile uint8_t buf[LOT_I2C0_BUF_SIZE];
 
 private:
-
 };
 
-#endif // _LOT_I2C0_H_
+extern LOT_i2c0 i2c0;
+
+#endif    // _LOT_I2C0_H_
