77370b21f00da05282b05ab63cc0be9151062532
Hyeonki Hong <hhk7734@gmail.com>
2018-12-23 02:25:33 +0900

    i2c 클래스

---
+++ LOT_i2c0/LOT_i2c0.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file LOT_i2c0.cpp
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief
+ */
+
+#include "LOT_i2c0.h"
+
+LOT_i2c0::LOT_i2c0()
+{
+
+}
---
+++ LOT_i2c0/LOT_i2c0.h
@@ -0,0 +1,22 @@
+/**
+ * @file LOT_i2c0.h
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief
+ */
+
+#ifndef _LOT_I2C0_H_
+#define _LOT_I2C0_H_
+
+#include <inttypes.h>
+
+class LOT_i2c0 {
+public:
+    LOT_i2c0();
+
+protected:
+
+private:
+
+};
+
+#endif // _LOT_I2C0_H_
