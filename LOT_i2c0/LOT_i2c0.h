e1f324e8545006279e438b6fccc076a9bc0ccef9
Hyeonki Hong <hhk7734@gmail.com>
2018-12-24 04:49:07 +0900

    함수 추가

--- LOT_i2c0/LOT_i2c0.h
+++ LOT_i2c0/LOT_i2c0.h
@@ -40,32 +40,43 @@ public:
     void        setup( const uint32_t scl_clock );
     inline void setup( void ) { setup( LOT_I2C0_DEFAULT_CLOCK ); }
 
-    void    transmit( const uint8_t device_address, const uint8_t *data, uint8_t size );
-    void    transmit( const uint8_t device_address, const uint8_t register_address, uint8_t data );
-    uint8_t receive( const uint8_t device_address, uint8_t *data, uint8_t max_size );
-    uint8_t receive( const uint8_t device_address,
+    uint8_t transmit( const uint8_t slave_address, const uint8_t *data, uint8_t size );
+    uint8_t transmit( const uint8_t slave_address,
+                      const uint8_t register_address,
+                      uint8_t *     data,
+                      uint8_t       size );
+    void    transmit( const uint8_t slave_address, const uint8_t register_address, uint8_t data );
+    uint8_t receive( const uint8_t slave_address, uint8_t *data, uint8_t max_size );
+    uint8_t receive( const uint8_t slave_address,
                      const uint8_t register_address,
                      uint8_t *     data,
                      uint8_t       max_size );
-    uint8_t receive( const uint8_t device_address, const uint8_t register_address );
+    uint8_t receive( const uint8_t slave_address, const uint8_t register_address );
 
-    void transmit_slow( const uint8_t device_address, const uint8_t *data, uint8_t size );
-    void
-            transmit_slow( const uint8_t device_address, const uint8_t register_address, uint8_t data );
-    uint8_t receive_slow( const uint8_t device_address, uint8_t *data, uint8_t max_size );
-    uint8_t receive_slow( const uint8_t device_address,
+    void transmit_slow( const uint8_t slave_address, const uint8_t *data, uint8_t size );
+    void transmit_slow( const uint8_t slave_address, const uint8_t register_address, uint8_t data );
+    uint8_t receive_slow( const uint8_t slave_address, uint8_t *data, uint8_t max_size );
+    uint8_t receive_slow( const uint8_t slave_address,
                           const uint8_t register_address,
                           uint8_t *     data,
                           uint8_t       max_size );
-    uint8_t receive_slow( const uint8_t device_address, const uint8_t register_address );
+    uint8_t receive_slow( const uint8_t slave_address, const uint8_t register_address );
 
 protected:
+private:
+    volatile uint8_t _slave_address_rw;
+    volatile uint8_t error_state;
+    volatile uint8_t state;
+
     volatile uint8_t buf_index;
     volatile uint8_t buf_length;
 
     volatile uint8_t buf[LOT_I2C0_BUF_SIZE];
 
-private:
+    void start( void );
+    void ack( void );
+    void nack( void );
+    void stop( void );
 };
 
 extern LOT_i2c0 i2c0;
