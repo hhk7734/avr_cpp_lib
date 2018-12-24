43239a7c46e2427ae6f91a7e876852018d321209
Hyeonki Hong <hhk7734@gmail.com>
2018-12-24 14:26:49 +0900

    setup 구상 중

--- LOT_spi0/LOT_spi0.cpp
+++ LOT_spi0/LOT_spi0.cpp
@@ -6,7 +6,13 @@
 
 #include "LOT_spi0.h"
 
-LOT_spi0::LOT_spi0()
-{
+#include <avr/io.h>
 
+LOT_spi0::LOT_spi0() {}
+
+void LOT_spi0::setup( LOT_spi_clk_divider_typedef       clk_divider,
+                      LOT_spi_data_order_typedef        data_order,
+                      LOT_spi_clk_idle_state_typedef    clk_idle,
+                      LOT_spi_clk_sampling_edge_typedef clk_sampling_edge )
+{
 }
