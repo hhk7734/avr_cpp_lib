9815e340d6da5d8c5e9ddfe5d427b3ac9ef0cfa4
Hyeonki Hong <hhk7734@gmail.com>
2018-12-24 11:44:50 +0900

    spi 클래스

---
+++ LOT_spi0/LOT_spi0.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file LOT_spi0.cpp
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief
+ */
+
+#include "LOT_spi0.h"
+
+LOT_spi0::LOT_spi0()
+{
+
+}
---
+++ LOT_spi0/LOT_spi0.h
@@ -0,0 +1,20 @@
+/**
+ * @file LOT_spi0.h
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief
+ */
+
+#ifndef _LOT_SPI0_H_
+#define _LOT_SPI0_H_
+
+#include <inttypes.h>
+
+class LOT_spi0 {
+public:
+    LOT_spi0();
+
+protected:
+private:
+};
+
+#endif    // _LOT_SPI0_H_
