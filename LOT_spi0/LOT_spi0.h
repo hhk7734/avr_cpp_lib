38388130611f7d0df7303a804f6eac0a1257b5d0
Hyeonki Hong <hhk7734@gmail.com>
2018-12-31 21:22:14 +0900

    readme

--- LOT_spi0/LOT_spi0.h
+++ LOT_spi0/LOT_spi0.h
@@ -61,8 +61,8 @@ public:
 
     void setup( LOT_spi_clk_divider_typedef       clk_divider,
                 LOT_spi_data_order_typedef        data_order,
-                LOT_spi_clk_idle_state_typedef    clk_idle,
                 LOT_spi_clk_sampling_edge_typedef clk_sampling_edge );
+                LOT_spi_clk_idle_state_typedef    clk_idle,
 
 protected:
 private:
---
+++ README.md
@@ -0,0 +1,3 @@
+![atmega328p](./002_pinout/atmega328.png)
+![atmega128](./002_pinout/atmega128.png)
+![atmega2560](./002_pinout/atmega2560.png)
