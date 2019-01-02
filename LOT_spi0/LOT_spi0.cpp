90d1ba643877ab51d3b081f4e7524a8cef44b3e7
Hyeonki Hong <hhk7734@gmail.com>
2019-01-02 14:15:02 +0900

    함수 추가

--- LOT_spi0/LOT_spi0.cpp
+++ LOT_spi0/LOT_spi0.cpp
@@ -27,7 +27,10 @@ const uint8_t LOT_SPIF { _BV( SPIF ) };
 const uint8_t LOT_WCOL { _BV( WCOL ) };
 const uint8_t LOT_SPI2X { _BV( SPI2X ) };
 
-LOT_spi0::LOT_spi0() {}
+LOT_spi0::LOT_spi0()
+    : error_count( 0 )
+{
+}
 
 void LOT_spi0::setup( LOT_spi_data_order_typedef        data_order,
                       LOT_spi_clk_idle_state_typedef    clk_idle,
@@ -37,6 +40,11 @@ void LOT_spi0::setup( LOT_spi_data_order_typedef        data_order,
     /// MOSI output
     /// MISO input
     /// SCK output
+#if defined( __AVR_ATmega328P__ )
+    DDRB |= _BV( DDB3 );
+    DDRB &= ~_BV( DDB4 );
+    DDRB |= _BV( DDB5 );
+#endif
 
     spcr = LOT_SPE | LOT_MSTR;
 
@@ -61,3 +69,101 @@ void LOT_spi0::setup( LOT_spi_data_order_typedef        data_order,
         case LOT_SPI_SCK_DIV_128: spcr |= LOT_SPR1 | LOT_SPR0; break;
     }
 }
+
+LOT_status_typedef
+    LOT_spi0::transmit( const uint8_t register_address, const uint8_t *data, uint8_t size )
+{
+    if( transmit( register_address ) == LOT_OK )
+    {
+        for( ; size > 0; --size ) { transmit( *data ); }
+        return LOT_OK;
+    }
+    else
+    {
+        error();
+        return LOT_ERROR;
+    }
+}
+
+inline LOT_status_typedef LOT_spi0::transmit( const uint8_t data )
+{
+    spdr = data;
+#if LOT_SPI0_TIME_OUT <= 0xFF
+    uint8_t count = LOT_SPI0_TIME_OUT;
+#else
+    uint16_t count = LOT_SPI0_TIME_OUT;
+#endif
+    while( ( spsr & LOT_SPIF ) == 0 )
+    {
+        --count;
+        if( count == 0 ) { return LOT_ERROR; }
+    }
+    return LOT_OK;
+}
+
+inline uint8_t LOT_spi0::transceive( const uint8_t data )
+{
+    transmit( data );
+    return spdr;
+}
+
+LOT_status_typedef LOT_spi0::transmit( const uint8_t register_address, const uint8_t data )
+{
+    transmit( register_address );
+    return transmit( data );
+}
+
+LOT_status_typedef LOT_spi0::receive( const uint8_t register_address, uint8_t *data, uint8_t size )
+{
+    if( transmit( register_address ) == LOT_OK )
+    {
+        for( ; size > 0; --size )
+        {
+            transmit( 0 );
+            *data++ = spdr;
+        }
+        return LOT_OK;
+    }
+    else
+    {
+        error();
+        return LOT_ERROR;
+    }
+}
+
+uint8_t LOT_spi0::receive( const uint8_t register_address )
+{
+    transmit( register_address );
+    transmit( 0 );
+    return spdr;
+}
+
+LOT_status_typedef LOT_spi0::transceive( uint8_t *data, uint8_t size )
+{
+    if( transmit( *data ) == LOT_OK )
+    {
+        for( ; size > 1; --size )
+        {
+            *data++ = spdr;
+            transmit( *data );
+        }
+        *data = spdr;
+        return LOT_OK;
+    }
+    else
+    {
+        error();
+        return LOT_ERROR;
+    }
+}
+
+void LOT_spi0::error( void )
+{
+    ++error_count;
+    if( error_count > 10 )
+    {
+        for( ;; ) {}
+    }
+}
+
+LOT_spi0 spi0;
--- LOT_spi0/LOT_spi0.h
+++ LOT_spi0/LOT_spi0.h
@@ -59,13 +59,35 @@ class LOT_spi0 {
 public:
     LOT_spi0();
 
-    void setup( LOT_spi_data_order_typedef        data_order,
-                LOT_spi_clk_idle_state_typedef    clk_idle,
-                LOT_spi_clk_sampling_edge_typedef clk_sampling_edge,
-                LOT_spi_clk_divider_typedef       clk_divider );
+    void        setup( LOT_spi_data_order_typedef        data_order,
+                       LOT_spi_clk_idle_state_typedef    clk_idle,
+                       LOT_spi_clk_sampling_edge_typedef clk_sampling_edge,
+                       LOT_spi_clk_divider_typedef       clk_divider );
+    inline void setup( LOT_spi_clk_divider_typedef clk_divider )
+    {
+        setup( LOT_SPI_MSB_FIRST, LOT_SPI_CLK_IDLE_LOW, LOT_SPI_CLK_SAMPLING_1_EDGE, clk_divider );
+    }
+
+    LOT_status_typedef transmit( const uint8_t data );
+    LOT_status_typedef
+                       transmit( const uint8_t register_address, const uint8_t *data, uint8_t size );
+    LOT_status_typedef transmit( const uint8_t register_address, const uint8_t data );
+
+    LOT_status_typedef receive( const uint8_t register_address, uint8_t *data, uint8_t size );
+    uint8_t            receive( const uint8_t register_address );
+
+    LOT_status_typedef transceive( uint8_t *data, uint8_t size );
+    uint8_t            transceive( const uint8_t data );
 
 protected:
 private:
+    uint8_t error_count;
+
+    /**
+     * @brief 통신 에러가 발생한 경우 처리하는 함수
+     */
+    void __attribute__( ( weak ) ) error( void );
 };
 
+extern LOT_spi0 spi0;
 #endif    // _LOT_SPI0_H_
