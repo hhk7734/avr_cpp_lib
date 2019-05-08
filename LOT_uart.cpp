1dacb91482c835b85a2a49eebecc5c4f3315ec6b
Hyeonki Hong <hhk7734@gmail.com>
2019-05-09 07:32:02 +0900

    ISR 이동

--- LOT_uart.cpp
+++ LOT_uart.cpp
@@ -98,3 +98,31 @@ uint8_t LOT_uart::gcount( void )
 {
     return ( LOT_UART_RX_BUF_SIZE + rx_buf_head - rx_buf_tail ) % LOT_UART_RX_BUF_SIZE;
 }
+
+#if defined( _USE_LOT_UART_0_ )
+
+#    if !defined( __AVR_ATmega328P__ )
+#        warning "Untested device"
+#    endif
+
+LOT_uart uart0( UCSR0A, UCSR0B, UCSR0C, UBRR0L, UBRR0H, UDR0 );
+
+#    if defined( USART_RX_vect )
+ISR( USART_RX_vect )
+#    elif defined( USART0_RX_vect )
+ISR( USART0_RX_vect )
+#    endif
+{
+    uart0.rx_isr();
+}
+
+#    if defined( USART_UDRE_vect )
+ISR( USART_UDRE_vect )
+#    elif defined( USART0_UDRE_vect )
+ISR( USART0_UDRE_vect )
+#    endif
+{
+    uart0.udre_isr();
+}
+
+#endif    /// _USE_LOT_UART_0_
