62c13bc9ba12712cecbdfc1bed14ad6605709709
Hyeonki Hong <hhk7734@gmail.com>
2019-05-08 19:00:09 +0900

    _BV 제거

--- LOT_uart_const.h
+++ LOT_uart_const.h
@@ -6,31 +6,31 @@
 #include <avr/io.h>
 
 /// UCSRA
-const uint8_t LOT_RXC  = _BV( RXC0 );
-const uint8_t LOT_TXC  = _BV( TXC0 );
-const uint8_t LOT_UDRE = _BV( UDRE0 );
-const uint8_t LOT_FE   = _BV( FE0 );
-const uint8_t LOT_DOR  = _BV( DOR0 );
-const uint8_t LOT_UPE  = _BV( UPE0 );
-const uint8_t LOT_U2X  = _BV( U2X0 );
-const uint8_t LOT_MPCM = _BV( MPCM0 );
+const uint8_t LOT_RXC  = RXC0;
+const uint8_t LOT_TXC  = TXC0;
+const uint8_t LOT_UDRE = UDRE0;
+const uint8_t LOT_FE   = FE0;
+const uint8_t LOT_DOR  = DOR0;
+const uint8_t LOT_UPE  = UPE0;
+const uint8_t LOT_U2X  = U2X0;
+const uint8_t LOT_MPCM = MPCM0;
 
 /// UCSRB
-const uint8_t LOT_RXCIE = _BV( RXCIE0 );
-const uint8_t LOT_TXCIE = _BV( TXCIE0 );
-const uint8_t LOT_UDRIE = _BV( UDRIE0 );
-const uint8_t LOT_RXEN  = _BV( RXEN0 );
-const uint8_t LOT_TXEN  = _BV( TXEN0 );
-const uint8_t LOT_UCSZ2 = _BV( UCSZ02 );
-const uint8_t LOT_RXB8  = _BV( RXB80 );
-const uint8_t LOT_TXB8  = _BV( TXB80 );
+const uint8_t LOT_RXCIE = RXCIE0;
+const uint8_t LOT_TXCIE = TXCIE0;
+const uint8_t LOT_UDRIE = UDRIE0;
+const uint8_t LOT_RXEN  = RXEN0;
+const uint8_t LOT_TXEN  = TXEN0;
+const uint8_t LOT_UCSZ2 = UCSZ02;
+const uint8_t LOT_RXB8  = RXB80;
+const uint8_t LOT_TXB8  = TXB80;
 
 /// UCSRC
-const uint8_t LOT_UMSEL1 = _BV( UMSEL01 );
-const uint8_t LOT_UMSEL0 = _BV( UMSEL00 );
-const uint8_t LOT_UPM1   = _BV( UPM01 );
-const uint8_t LOT_UPM0   = _BV( UPM00 );
-const uint8_t LOT_USBS   = _BV( USBS0 );
-const uint8_t LOT_UCSZ1  = _BV( UCSZ01 );
-const uint8_t LOT_UCSZ0  = _BV( UCSZ00 );
-const uint8_t LOT_UCPOL  = _BV( UCPOL0 );
+const uint8_t LOT_UMSEL1 = UMSEL01;
+const uint8_t LOT_UMSEL0 = UMSEL00;
+const uint8_t LOT_UPM1   = UPM01;
+const uint8_t LOT_UPM0   = UPM00;
+const uint8_t LOT_USBS   = USBS0;
+const uint8_t LOT_UCSZ1  = UCSZ01;
+const uint8_t LOT_UCSZ0  = UCSZ00;
+const uint8_t LOT_UCPOL  = UCPOL0;
