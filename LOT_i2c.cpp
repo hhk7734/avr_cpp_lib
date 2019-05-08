760fa281a0f930560f7886c8a4ecb7739c4c6adc
Hyeonki Hong <hhk7734@gmail.com>
2019-05-09 05:01:40 +0900

    I2C

---
+++ LOT_i2c.cpp
@@ -0,0 +1,7 @@
+/**
+ * @file LOT_i2c.cpp
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief AVR I2C 마스터 통신
+ */
+
+#include "LOT_i2c.h"
---
+++ LOT_i2c.h
@@ -0,0 +1,18 @@
+/**
+ * @file LOT_i2c.h
+ * @author Hyeon-ki, Hong (hhk7734@gmail.com)
+ * @brief AVR I2C 마스터 통신
+ */
+
+#ifndef _LOT_I2C_H_
+#define _LOT_I2C_H_
+
+#include <inttypes.h>
+
+class LOT_i2c {
+    public:
+    protected:
+    private:
+};
+
+#endif // _LOT_I2C_H_
